27f93e016b9e114cb1ad2f49cea733aea22103e4
rodizaf <49204913+rodizaf@users.noreply.github.com>
2020-03-31 15:53:23 +0300

    Add files via upload

---
+++ Lesson1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBasics
+{
+    class Lesson1
+    {
+        static void Main(string[] args)
+        {
+            #region Lesson 1 youtube
+            Console.WriteLine("Please Enter Your Name : ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Hello {input}");
+            Console.WriteLine("Please Choose Number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine($"Num {a} is even");
+            }
+            else
+            {
+                Console.WriteLine($"Num {a} is odd");
+            }
+            Console.WriteLine($"You Have Chosen : {a}");
+            int t = 1;
+            while (t <= a)
+            {
+                Console.WriteLine(t);
+                t++;
+            }
+            #endregion
+
+            #region  1st exercise
+            Console.WriteLine("Enter the first number");
+            int f = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (f % b == 0)
+            {
+                Console.WriteLine($"Number : {f} is divided by {b} without the remainder");
+            }
+            else
+            {
+                Console.WriteLine($"Number : {f} can't be divide by {b} without the remainder");
+            }
+            #endregion
+
+            #region  2nd exercise
+            Console.WriteLine("Enter the first number");
+            int aa = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int bb = Convert.ToInt32(Console.ReadLine());
+            int result = aa + bb;
+            float avg = result / 2;
+            Console.WriteLine($"Result of {aa} + {bb} is {result} \n\nThe average of {aa} and {bb} is {avg}");
+            #endregion
+
+            #region 3rd exercise
+            int sum = 0;
+            Console.WriteLine("Please enter a number: ");
+            int i = Convert.ToInt32(Console.ReadLine());         //3
+            while (i >= 0)
+            {
+                Console.WriteLine($"You chose {i} which is positive number");
+                sum = sum + i;    //adds i to the sum each time
+                Console.WriteLine($"Now the sum is : {sum}");
+                Console.WriteLine("Choose another number: ");
+
+                i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"The sum is : {sum}");
+                if (i < 0)
+                {
+                    Console.WriteLine($"{i} is Negative");
+                }
+            }
+            #endregion
+
+            #region 4th exercise
+            Console.WriteLine("Please Enter a number:  ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            int m = 2;
+            while (d % m != 0 && m < d)
+            {
+                m++;
+            }
+            if (m < d)
+            {
+                Console.WriteLine("Lo rishoni!!");
+
+            }
+            else
+            {
+                Console.WriteLine("Rishoni");
+            }
+
+            #endregion
+
+            #region 5th exercise
+            Console.WriteLine("Please enter your secret number: ");
+
+            int secret = Convert.ToInt32(Console.ReadLine());
+            // int secret = (new Random()).Next(1, 100);
+            Console.WriteLine("Please enter guess: ");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            while (secret != guess)
+            {
+                if (guess < secret)
+                {
+                    Console.WriteLine("Too small");
+                }
+                else
+                {
+                    Console.WriteLine("Too big");
+                }
+                Console.WriteLine("Please enter another guess: ");
+                guess = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("Bingo!");
+            #endregion
+
+            #region 6th exercise
+            Console.WriteLine("Please enter a number: ");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int i2 = 2;
+            int atz = 1;
+            while (i2 <= a2)                             // איך עושים עצרת באנגלית : factorial
+            {
+                atz = atz * i2;
+                i2++;
+            }
+            Console.WriteLine(atz);
+            #endregion
+
+            #region 7th exercise
+            Console.WriteLine("Please enter a:");
+            int a3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter b:");
+            int b3 = Convert.ToInt32(Console.ReadLine());
+            int i3 = 1;
+            int sum3 = 0;
+            while (i3 <= a3)                  // איך לעשות כפל בלי להשתמש בכפל
+            {
+                sum = sum + b3;
+                i3++;
+            }
+            Console.WriteLine($"the Sum of a & b is: {sum3}");
+            #endregion
+
+        }
+    }
+}
---
+++ Lesson2.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DotNetBasics
+{
+    class Lesson2
+    {
+      
+        static void Main(string[] args)
+        {
+
+            #region Exercise 1 for's
+            for (int i = 1; i <= 2000; i++)            //1
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 2; i <= 100; i += 2)        //1.a
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 10; i <= 100; i += 10)       // 1.b
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
+
+            #region Exercise 2 for's
+            for (int i = 100; i >= 1; i--)
+            {
+                Console.WriteLine(i);       //2
+            }
+            #endregion
+
+            #region Exercise 3 if's
+            Console.WriteLine("Please Choose a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a >= 10 && a <= 99)
+            {
+                Console.WriteLine($"You Choose: {a}");
+
+                int ahadot = a % 10;
+                Console.WriteLine($"ahddot is {ahadot}");     //3
+
+                int asarot = a / 10;
+                Console.WriteLine($"asarot is {asarot}");
+
+                if (asarot > ahadot)
+                {
+                    Console.WriteLine("ASAROT!!");
+                }
+                else
+                {
+                    Console.WriteLine("AHDAOT!!!!!!");
+                }
+            }
+            #endregion
+
+            #region Exercise 4 PrimeNumbers
+            for (int j = 1; j <= 100; j++)
+            {
+                int m = 2;
+                int x = j;
+
+                while (x % m != 0 && m < x)
+                {
+                    m++;
+                }
+                if (x == m || x == 1)
+                {                                         // 4
+                    Console.WriteLine($"Rishoni: {j}");   // getting all prime numbers from 1-100
+
+                }
+                else
+                {
+                    Console.WriteLine($"Lo Rishoni: {j}");
+                }
+            }
+            #endregion
+
+            #region Exercise 5 Dividing money into it's bills value
+            Console.WriteLine("Please enter the sum of money: ");
+            int money = Convert.ToInt32(Console.ReadLine());
+            if (money>=200)
+            {
+                int shtarotMataim = money / 200;
+                money = money - shtarotMataim * 200;
+                Console.WriteLine($"200$: {shtarotMataim}");
+            } if (money>=100)
+            {
+                int shtarotMea = money / 100;
+                money = money - shtarotMea * 100;
+                Console.WriteLine($"100$: {shtarotMea}");
+            } 
+            if (money>=50)
+            {
+                int shtarotHamishim = money / 50;
+                money = money - shtarotHamishim * 50;
+                Console.WriteLine($"50$: {shtarotHamishim}");
+            } 
+            if (money>=20)
+            {
+                int shtarotEsrim = money / 20;
+                money = money - shtarotEsrim * 20;
+                Console.WriteLine($"20$: {shtarotEsrim}");
+            } 
+            if (money>=10)
+            {
+                int asiriyot = money / 10;
+                money = money - asiriyot * 10;
+                Console.WriteLine($"10$: {asiriyot}");
+            } 
+            if (money>=5)
+            {
+                int hamishiyot = money / 5;
+                money = money - hamishiyot * 5;
+                Console.WriteLine($"5$: {hamishiyot}");
+            } 
+            if (money>=2)
+            {
+                int shnekelim = money / 2;
+                money = money - shnekelim * 2;
+                Console.WriteLine($"2$: {shnekelim}");
+            }  
+            if (money>=1)
+            {
+                int shkalim = money / 1;
+                money = money - shkalim * 1;
+                Console.WriteLine($"1$: {shkalim}");
+            }
+            #endregion
+
+            #region Exercise 6 Printing triangle using nums/*'s
+            Console.WriteLine("Please enter a number you like: ");
+            int chosenNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Result:");
+            for (int i = 1; i <= chosenNum; i++)
+            {
+                for (int j = 1; j <= chosenNum; j++)
+                {
+                    if (j > i)
+                    {
+                        Console.Write('*');              //version 1
+                    }
+                    else
+                    {
+                        Console.Write(j);
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Result:");
+            for (int i2 = 1; i2 <= chosenNum; i2++)
+            {
+                for (int j = 1; j <= i2; j++)
+                {
+                    Console.Write(j);
+                }
+                                                              //version 2 without if
+                for (int j = i2 + 1; j <= chosenNum; j++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+
+            #endregion
+
+        }
+
+
+
+    }
+    }
+
