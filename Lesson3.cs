646c198b6d739a831cc82509ce241657a9c8df04
rodizaf <49204913+rodizaf@users.noreply.github.com>
2020-04-01 16:17:29 +0300

    Lesson 3

---
+++ Lesson3.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    class Lesson3:myFirstFunction
+    {
+        
+        static void Main(string[] args)
+        {
+            DividingStudentsToClassesMethod(); // EX 2
+            MyPrimeMethod(); // EX 1
+            NewMethod();    // Triangle 1st Function
+            TriangleFuncUpdate(); // Ex 3
+            TriangleOfStarsFunc();
+
+            //    #region Card Game
+
+            //    Console.WriteLine(" Hello dear guest!! \n Welcome to Battle Card Game");
+            //    Console.WriteLine(" If you are ready to play please double press \"Enter\"\n");
+            //    Console.ReadLine();
+
+            //    int myScore = 0;
+            //    int computerScore = 0;
+
+            //    while (myScore < 5 && computerScore < 5)
+            //    {
+            //        //whos bigger card1>card2?
+            //        Console.WriteLine("My Card: ");
+
+            //        //me vrs computer get random card
+
+            //        Random r = new Random();
+            //        int card = r.Next(2, 15);
+
+
+            //        PrintCard(card);
+
+
+            //        // go to sleep
+            //        System.Threading.Thread.Sleep(300);
+
+            //        Console.WriteLine("Computer Card:");
+            //        // me vrs computer
+            //        // get random card
+            //        int card1 = r.Next(2, 15);
+
+            //        // print the card
+            //        PrintCard(card1);
+
+            //        // check who won
+            //        if (card > card1)
+            //        {
+            //            // i won
+            //            myScore++;
+            //            Console.WriteLine("Player won!");
+            //        }
+            //        else if (card == card1)
+            //        {
+            //            // tie
+            //            Console.WriteLine("Its a tie!!!");
+            //        }
+            //        else
+            //        {
+            //            // computer won
+            //            computerScore++;
+            //            Console.WriteLine("Computer won!");
+            //        }
+            //        Console.WriteLine($"My score = {myScore}");
+            //        Console.WriteLine($"Computer score = {computerScore}");
+
+            //        if (myScore != 5 && computerScore != 5)
+            //        {
+            //            Console.WriteLine("Ready for another round? press \"Enter\"");
+            //            Console.ReadLine();
+            //        }
+
+            //    }
+            //    Console.WriteLine("--- Game Over ---");
+
+            //}
+
+            //private static void PrintCard(int card)
+            //{
+            //    switch (card)
+            //    {
+            //        case 11:
+            //            Console.WriteLine("J");
+            //            break;
+            //        case 12:
+            //            Console.WriteLine("Q");
+            //            break;
+            //        case 13:
+            //            Console.WriteLine("K");
+            //            break;
+            //        case 14:
+            //            Console.WriteLine("A");
+            //            break;
+            //        case 15:
+            //            Console.WriteLine("JOKER!!");
+            //            break;
+            //        default:
+            //            Console.WriteLine(card);
+            //            break;
+            //    }
+        }
+        //#endregion
+
+
+
+    }
+}
---
+++ myFirstFunction.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+   public class myFirstFunction
+    {
+        #region 1stFunction making Triangle from numbers
+        public static void NewMethod()
+        {
+            Console.WriteLine("doing work...");
+            Console.WriteLine("Enter number for triangle:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+
+            }
+        }
+        #endregion
+
+        #region Hw Exercise 1
+        public static void MyPrimeMethod()
+        {
+            Console.WriteLine("PrimeMethod working...");
+            Console.WriteLine("Please enter a number of your choice: ");
+            int m = 0;
+            int x = 0;
+            while (x == m)
+            {
+
+                m = 2;
+                x = Convert.ToInt32(Console.ReadLine());
+
+
+                while (x % m != 0 && m < x)
+                {
+                    m++;
+                }
+                if (x > m)
+                {
+                    Console.WriteLine($"Lo Rishoni: {x}");
+                }
+                else
+                {
+                    Console.WriteLine($"Rishoni: {x}");
+
+                }
+            }
+
+        }
+#endregion
+
+        #region Hw Exercise 2
+        public static void DividingStudentsToClassesMethod()
+        {
+            Console.WriteLine("Please enter the number of students: ");
+            int numberOfStudents = Convert.ToInt32(Console.ReadLine());
+            
+            if (numberOfStudents>=100)
+            {
+                int kitotShelMea = numberOfStudents / 100;
+                numberOfStudents = numberOfStudents - kitotShelMea * 100;
+                Console.WriteLine($"{kitotShelMea} Classes of 100 students");
+            }   
+            if (numberOfStudents>=30&&numberOfStudents<100)
+            {
+                int kitotShelShloshim = numberOfStudents / 30;
+                numberOfStudents = numberOfStudents - kitotShelShloshim * 30;
+                Console.WriteLine($"{kitotShelShloshim} Classes of 30 students");
+            }  
+            if (numberOfStudents>=10&&numberOfStudents<30)
+            {
+                int kitotShelEser = numberOfStudents / 10;
+                numberOfStudents = numberOfStudents - kitotShelEser * 10;
+                Console.WriteLine($"{kitotShelEser} Classes of 10 students");
+            }
+            if (numberOfStudents>=1&&numberOfStudents<10)
+            {
+                int talmidimBodedim = numberOfStudents / 1;
+                numberOfStudents = numberOfStudents - talmidimBodedim * 1;
+                Console.WriteLine($"{talmidimBodedim} students went home");
+            }
+        }
+        #endregion
+        #region Hw Exercise 3
+        public static void TriangleFuncUpdate()
+        {
+            Console.WriteLine("Please choose number for the triangle: ");
+            int rows = Convert.ToInt32(Console.ReadLine()); // read a number
+            // run from 1 -> rows
+            for (int i = 1; i <= rows; i++)
+            {
+
+                // print all numbers from 1 to i
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j); // print j do not line feed
+                }
+
+                Console.WriteLine(); // line feed
+            }
+            for (int i = rows - 1; i >= 1; i--)
+            {
+
+                // print all numbers from 1 to i
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j); // print j do not line feed
+                }
+
+                Console.WriteLine(); // line feed
+            }
+        }
+        #endregion 
+
+        public static void TriangleOfStarsFunc()
+        {
+            int stars = Convert.ToInt32(Console.ReadLine()); // read a number
+
+            int i = 1; // number of starts i am drawing in the current row
+
+            int spaces = stars / 2; // number of spaces in each row
+
+            while (i <= stars)
+            {
+                // draw the spaces
+                for (int j = 1; j <= spaces; j++)
+                {
+                    Console.Write(' ');
+                }
+
+                // draw the stars
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+
+                // add 2 to i
+                i = i + 2;
+
+                // decrease 1 to spaces
+                spaces = spaces - 1;
+            }
+        }
+
+    }
+
+
+}
+
